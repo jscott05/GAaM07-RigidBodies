7b4108334aa0dcae1d2871c334de227e59c5e9df
Jarrad Scott <jarrad-scott2005@outlook.com>
2025-10-24 15:28:41 +0100

    Made Functional

--- GAaM07-RigidBodies.cpp
+++ GAaM07-RigidBodies.cpp
@@ -207,6 +207,7 @@ public:
     void update(float deltaTime)
     {
         //auto - dont know type, figure it out
+        //apply gravity to all bodies - stage 1
         for (auto& body : bodies)
         {
             if (!body->isStatic && !body->isResting)
@@ -215,20 +216,22 @@ public:
             }
         }
 
-        //update pos
+        //update positions - stage 2
         for (auto& body : bodies)
         {
             body->update(deltaTime);
             body->checkBoundaryCollision(worldWidth, worldHeight);
         }
 
+        //check for collisions between bodies - stage 3
+        // O(n^2) -- not entirely efficient
         for (size_t i = 0; i < bodies.size(); ++i) //pre increment i
         {
             for (size_t j = i + 1; j < bodies.size(); ++j) // pre increment j
             {
                 //do collision checks - comparing every particle to every other particle
                 //optimisation - look at data structures and algorithms - bounding / volume
-
+                checkCollision(*bodies[i], *bodies[j]);
             }
         }
     }
@@ -242,6 +245,12 @@ public:
 
         if (distance < minDistance)
         {
+            //floating point bs -- prevent division by 0 for overlaps in bodies
+            if (distance < 0.001)
+            {
+                distance = 0.001f;
+                difference = sf::Vector2f(1.0f, 0.0f) * minDistance;
+            }
             //wake up resting bodies
             body1.wake();
             body2.wake();
@@ -251,10 +260,12 @@ public:
 
             //separate the bodies
             float overlap = minDistance - distance;
+            float seperationFactor = 1.01f;
+
             if (!body1.isStatic && !body2.isStatic)
             {
-                body1.position -= normal * (overlap * 0.5f);
-                body2.position += normal * (overlap * 0.5f);
+                body1.position -= normal * (overlap * 0.5f * seperationFactor);
+                body2.position += normal * (overlap * 0.5f * seperationFactor);
             }
             else if (!body1.isStatic)
             {
@@ -283,7 +294,7 @@ public:
 
             if (!body1.isStatic && !body2.isStatic)
             {
-                j /= (1 / body1.mass / body2.mass);
+                j /= (1 / body1.mass +1 / body2.mass);
             }
             else if (!body1.isStatic)
             {
@@ -369,12 +380,28 @@ public:
     }
 
     //Next Session - Figure out call finding bodies at specific locations (so boundaries / volume check??)
+
+    RigidBody* getBodyAt(const sf::Vector2f& point)
+    {
+        for (auto& body : bodies)
+        {
+            sf::Vector2f diff = body->position - point;
+            if (length(diff) <= body->radius)
+            {
+                return body.get();
+            }
+        }
+        return nullptr; // it expects a pointer return to a rigidbody, so must return to a null pointer as opposed to null
+    }
 };
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode({ 1200,800 }), "Rigid Bodies");
     window.setFramerateLimit(60);
+
+    //Init Phys Engine
+    PhysicsEngine physics(1200.0f, 800.0f);
 
     //Rand Num Gen
     std::random_device rd;
@@ -383,18 +410,160 @@ int main()
     std::uniform_real_distribution<float> posX(100.0f, 1100.0f);
     std::uniform_real_distribution<float> posY(100.0f, 300.0f);
     std::uniform_real_distribution<float> radiusDist(15.0f, 40.0f);
-    std::uniform_real_distribution<int> colourDist(100, 255);
+    std::uniform_real_distribution<float> massDist(1.0f, 5.0f);
+    std::uniform_int_distribution<int>   colourDist(100, 255);
 
     sf::Font font;
     font.openFromFile("C:/Windows/Fonts/arial.ttf");
-    sf::Text ot(font, "");
+    sf::Text it(font, "Controls: "
+        "\nLeft Click - Spawn Particle"
+        "\nRight Click - Drag Particle"
+        "\nSpace - Spawn 5 Particles"
+        "\nC - Clear Particles"
+        "\nG - Toggle Gravity");
 
+    //Add Static Objs For Collisions
+    physics.addBody(std::make_unique<RigidBody>(sf::Vector2f(300.0f, 500.0f), 50, 10, sf::Color(100, 100, 100), true));
+    physics.addBody(std::make_unique<RigidBody>(sf::Vector2f(600.0f, 400.0f), 50, 10, sf::Color(100, 100, 100), true));
+    physics.addBody(std::make_unique<RigidBody>(sf::Vector2f(900.0f, 500.0f), 50, 10, sf::Color(100, 100, 100), true));
+
+    //Add Initial Dyn Bodies
+    for (int i = 0; i < 10; ++i)
+    {
+        float radius = radiusDist(gen);
+        float mass = massDist(gen) * (radius/20.0f);
+        sf::Color colour(colourDist(gen), colourDist(gen), colourDist(gen));
+        sf::Vector2f position(posX(gen), posY(gen));
+
+        physics.addBody(std::make_unique<RigidBody>(position, radius, mass, colour));
+
+    }
+
+    //Drag Stuff | Mouse Interaction Variables
+    RigidBody* draggedBody = nullptr;
+    sf::Vector2f dragOffset;
+
+    //Delta Time Clock
     sf::Clock clock;
     while (window.isOpen())
     {
         float deltaTime = clock.restart().asSeconds();
+
+        //Event Loop
+        while (const std::optional event = window.pollEvent())
+        {
+            //close window on exit event
+            if (event->is<sf::Event::Closed>())
+            {
+                std::cout << "Window Closing";
+                window.close();
+            }
+
+            //mouse button pressed
+            if (const auto* mousePressed = event->getIf<sf::Event::MouseButtonPressed>())
+            {
+                //cast into vector
+                sf::Vector2f mousePos(mousePressed->position.x, mousePressed->position.y);
+
+                //Grab particle and drag
+                if (mousePressed->button == sf::Mouse::Button::Right)
+                {
+                    draggedBody = physics.getBodyAt(mousePos);
+                    if (draggedBody)
+                    {
+                        dragOffset = draggedBody->position - mousePos;
+                        draggedBody->wake(); // objects sleep so that we dont check if they hit objects, as they are not moving - optimisation
+                    }
+                }
+                //Spawn Particles
+                if (mousePressed->button == sf::Mouse::Button::Left)
+                {
+                    float radius = radiusDist(gen);
+                    float mass = massDist(gen) * (radius / 20.0f);
+                    sf::Color colour(colourDist(gen), colourDist(gen), colourDist(gen));
+
+                    physics.addBody(std::make_unique<RigidBody>(mousePos, radius, mass, colour));
+                }
+            }
+
+            //on mouse release
+            if (event->is<sf::Event::MouseButtonReleased>())
+            {
+                //if dragged body exists, reset it to a nullptr
+                if (draggedBody) draggedBody = nullptr;
+            }
+
+            if (const auto* mouseMoved = event->getIf<sf::Event::MouseMoved>())
+            {
+                if (draggedBody && !draggedBody->isStatic)
+                {
+                    //cast into vector
+                    sf::Vector2f mousePos(mouseMoved->position.x, mouseMoved->position.y);
+
+                    sf::Vector2f targetPos = mousePos + dragOffset;
+
+                    draggedBody->velocity = (targetPos - draggedBody->position) * 10.0f;
+                }
+            }
+
+            //Key Press Events
+            if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>())
+            {
+                //Space Key Scancode
+                if (keyPressed->scancode == sf::Keyboard::Scancode::Space)
+                {
+                    //Spawn a lot of particles
+                    for (int i = 0; i < 5; ++i)
+                    {
+                        float radius = radiusDist(gen);
+                        float mass = massDist(gen) * (radius / 20.0f);
+                        sf::Color colour(colourDist(gen), colourDist(gen), colourDist(gen));
+                        sf::Vector2f position(posX(gen), posY(gen));
+
+                        physics.addBody(std::make_unique<RigidBody>(position, radius, mass, colour));
+
+                    }
+                }
+                //C Key Scancode
+                if (keyPressed->scancode == sf::Keyboard::Scancode::C)
+                {
+                    physics.clearDynamicBodies();
+                }
+
+                //G Key Scancode
+                if (keyPressed->scancode == sf::Keyboard::Scancode::G)
+                {
+                    static bool gravityOn = true;
+                    gravityOn = !gravityOn;
+
+                    physics.setGravity(gravityOn ? sf::Vector2f(0.0f, 500.0f) : sf::Vector2f(0.0f, 0.0f));
+                }
+            }
+        }
+
+        //Update Physics
+        physics.update(deltaTime);
+
+        //Render stuff
+        window.clear(sf::Color(20,20,30));
+        physics.draw(window);
+        window.draw(it);
+        window.display();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
